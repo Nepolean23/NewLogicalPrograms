a5fafcb0bfcb596262db76b9c16713e1cff28be0
Nepolean23 <nlean1996@gmail.com>
2023-01-06 12:33:26 +0530

    [Nepolean]-Code for FibonacciSeries Program

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public static void Finonaccinumbers()
+        {
+            int sum, f1 = 0, f2 = 1;
+            Console.WriteLine("Fibonacci Numbers Programs");
+            Console.Write("Enter the nth term for fibonacci series :");
+            int num=Convert.ToInt32(Console.ReadLine());
+            Console.Write("0 + 1");
+            for(int i=0; i<=num; i++) 
+            {
+                sum = f1 + f2;
+                f1 = f2;
+                f2= sum;
+                Console.Write(" + "+sum);
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using LogicalPrograms;
+
+Console.WriteLine("1.FibonnaciNumbers");
+Console.WriteLine("Enter your option to get in to above Programs:");
+int option=Convert.ToInt32(Console.ReadLine());
+switch(option)
+{
+    case 1:
+        FibonacciSeries.Finonaccinumbers();
+        break;
+}
