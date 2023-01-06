cadc1a1bda960d2a8f341eb7c223899066e2a5f2
Nepolean23 <nlean1996@gmail.com>
2023-01-06 17:15:54 +0530

    [Nepolean]-Code for Reverse Number Program

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,21 +1,25 @@
 ﻿
 using LogicalPrograms;
 
-Console.WriteLine("1.FibonnaciNumbers,2.PerfectNumbers,3.PrimeNumbers");
+Console.WriteLine("1.FibonnaciNumbers,2.PerfectNumbers,3.PrimeNumbers,4.ReverseNumber");
 Console.WriteLine("Enter your option to get in to above Programs:");
-int option=Convert.ToInt32(Console.ReadLine());
-switch(option)
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
 {
     case 1:
         FibonacciSeries.Finonaccinumbers();
         break;
 
     case 2:
-        PerfectNumber.Checkingperfectnumbers(); 
+        PerfectNumber.Checkingperfectnumbers();
         break;
 
     case 3:
         PrimeNumbers.Checkingprimenumbers();
+        break;
+
+    case 4:
+        ReverseNumber.Checkingreversenumber();
         break;
 
 
---
+++ LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class ReverseNumber
+    {
+        public static void Checkingreversenumber()
+        {
+            int reverseNumber=0,rem;
+            Console.WriteLine("Reverse Number Program");
+            Console.Write("Enter a number to check its Reverse Number :");
+            int num=Convert.ToInt32(Console.ReadLine());
+            while (num!=0)
+            {
+                rem = num % 10;
+                reverseNumber = reverseNumber * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("The Reversed Number is :"+reverseNumber);
+
+
+        }
+    }
+}
