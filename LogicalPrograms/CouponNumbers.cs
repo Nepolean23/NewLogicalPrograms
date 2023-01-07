34f42ad41232fe207d7b1cdbc2b57fb8356a8447
Nepolean23 <nlean1996@gmail.com>
2023-01-07 19:24:16 +0530

    [NEPOLEAN]-Code for Coupon Numbers Program

---
+++ LogicalPrograms/CouponNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class CouponNumbers
+    {
+        public static void Generatingnumbers()
+        {
+            Console.WriteLine("How many times do you need to generate a Coupon Code");
+            int Coupon = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=Coupon;i++) 
+            {
+                var chars = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst1234567890";
+                var stringchars = new char[6];
+                var random = new Random();
+
+                for(int j=0;j<stringchars.Length;j++) 
+                {
+                    stringchars[j] = chars[random.Next(stringchars.Length)];
+                }
+
+                var coupon=new string(stringchars);
+                Console.WriteLine("Generating Coupon number {0} is {1}",i, coupon);
+            }
+            
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using LogicalPrograms;
 
-Console.WriteLine("1.FibonnaciNumbers,2.PerfectNumbers,3.PrimeNumbers,4.ReverseNumber");
+Console.WriteLine("1.FibonnaciNumbers,2.PerfectNumbers,3.PrimeNumbers,4.ReverseNumber,5.CouponNumbers");
 Console.WriteLine("Enter your option to get in to above Programs:");
 int option = Convert.ToInt32(Console.ReadLine());
 switch (option)
@@ -20,6 +20,10 @@ switch (option)
 
     case 4:
         ReverseNumber.Checkingreversenumber();
+        break;
+
+    case 5:
+       CouponNumbers.Generatingnumbers();
         break;
 
 
